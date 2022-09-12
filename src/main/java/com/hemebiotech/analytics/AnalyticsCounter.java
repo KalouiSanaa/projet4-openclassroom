95e2a695d059176d8057f3eb1b255758af075d93
kaloui sanaa <marwa_elkal@hotmail.com>
2022-09-12 12:32:58 +0200

    version 2

--- src/main/java/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/main/java/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,4 +1,5 @@
 package com.hemebiotech.analytics;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.TreeMap;
@@ -8,40 +9,37 @@ import java.util.TreeMap;
  * @author Kaloui sanaa
  */
 public class AnalyticsCounter {
-    /**
-     *
-     */
     private static List<String> list;
      private static TreeMap<String, Integer> symptomsOccurence;
 
- 	/**
- 	 * @param args
- 	 * @throws Exception
- 	 */
  	public static void main(String[] args) throws Exception {
 
 		
-		//creer une listes des symptomes.
+		//creer une liste des symptomes.
 		
 	 ReadSymptomDataFromFile create = new ReadSymptomDataFromFile("symptoms.txt");
 
 		 list = create.GetSymptoms();
+                 
+                 
+                //Créer une liste de tri.
                 
                 symptomsOccurence = count(list);
                 
+                //Regardez si le tri a fonctionné.
+                
                showList();
+               
+               //Générer un fichier avec les résultats.
                 
                 AnalyticsSortie file = new AnalyticsSortie(symptomsOccurence);
                 
-		                     file.Sortie();
-
+		file.Sortie();
+                
+                
                		//Créer une liste de tri.
     }
         
-        /**
-         * @param list
-         * @return
-         */
         public static TreeMap<String, Integer> count(List<String> list) {
             
    symptomsOccurence = new TreeMap<>();
@@ -50,10 +48,12 @@ public class AnalyticsCounter {
       symptomsOccurence.put(line, Collections.frequency(list, line));
     }
     return symptomsOccurence;
+    /** return une liste de tri par ordre alphanumérique
+ */
   }
         /**
-         * 
-         */
+L'affichage dans le shell si le tri a fonctionné. */
+        
         public static void showList() {
 		System.out.println("avant tri " + list);
 		System.out.println("apres tri : " + symptomsOccurence);
